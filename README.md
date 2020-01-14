da26c680308254e1dad1d2afc730aa9865a76aa2
Imraan Ali <imraan91@hotmail.co.uk>
2020-01-14 15:54:41 +0000

    able to view bookmark contents on local host

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bookmark_challenge
+
+## how to install database
+Connect to psql
+Create the database using the psql command CREATE DATABASE bookmark_manager;
+Connect to the database using the pqsl command \c bookmark_manager;
+Run the query we have saved in the file 01_create_bookmarks_table.sql
--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,11 +1,20 @@
+require 'pg'
 class Bookmark
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-     ]
-  end
 
+    begin
+        con = PG.connect :dbname => 'bookmark_manager', :user => ENV['USER']
+        rs = con.exec "SELECT * FROM bookmarks"
+        list = []
+        rs.each {|row| list << row['url']}
+
+    rescue PG::Error => e
+        puts e.message
+    ensure
+        rs.clear if rs
+        con.close if con
+    end
+    list
+  end
 end
